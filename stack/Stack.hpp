4edaca945066972adbd8b4a113cc2e3de1b11e57
AarhamH <ahaider@cayenta.com>
2024-12-04 23:55:45 -0800

    Add debugging print methods and provide proper documentation

--- stack/Stack.hpp
+++ stack/Stack.hpp
@@ -5,12 +5,25 @@ class Stack {
   private:
     int* m_stack;
     int m_size;
+    int m_buffer_length;
   
   public:
     // construct an empty stack instance
     Stack();
 
+    /// @brief Checks if the stack is empty (size is 0)
+    /// @return True if empty, false otherwise
     bool is_empty();
+
+    /// @brief  Pushes an item to the front of the stack
+    /// @param item is the item to be pushed into stack
+    void push(int item);
+
+    /// @brief Prints the stack data structure
+    void print_stack();
+
+    /// @brief Prints the full buffer allocated in the heap (for debugging)
+    void print_full_buffer();
     
     ~Stack();
 };
