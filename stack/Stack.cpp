c70dede542ebb684ececd464079b5e1f4c03dc38
AarhamH <ahaider@cayenta.com>
2024-12-05 00:27:59 -0800

    Implemented stack pop and clear

--- stack/Stack.cpp
+++ stack/Stack.cpp
@@ -11,14 +11,36 @@ bool Stack::is_empty() {
   return m_size == 0;
 }
 
+int Stack::size() {
+  return m_size;
+}
+
 void Stack::push(int item) {
   if(m_size >= m_buffer_length) {
-    m_buffer_length += 5;
+    m_buffer_length += STACK_GROW_SHRINK_LIMIT;
     m_stack = (int*)realloc(m_stack, m_buffer_length * sizeof(int));
   }
 
   m_stack[m_size] = item;
   m_size++;
+}
+
+void Stack::pop() {
+  if(m_size > 0) {
+    m_stack[m_size] = 0;
+    m_size--;
+  
+    if(m_size <= (m_buffer_length - STACK_GROW_SHRINK_LIMIT) || m_size <= 0) {
+      m_buffer_length = m_size;
+      m_stack = (int*)realloc(m_stack, m_buffer_length * sizeof(int));
+    }
+  }
+}
+
+void Stack::clear() {
+  m_size = 0;
+  m_buffer_length = 0;
+  m_stack = (int*)realloc(m_stack, 0);
 }
 
 void Stack::print_stack() {
--- stack/Stack.hpp
+++ stack/Stack.hpp
@@ -1,5 +1,6 @@
 #ifndef STACK_H
 #define STACK_H
+#define STACK_GROW_SHRINK_LIMIT 5
 
 class Stack {
   private:
@@ -8,16 +9,26 @@ class Stack {
     int m_buffer_length;
   
   public:
-    // construct an empty stack instance
+    /// @brief Allocates an empty stack 
     Stack();
 
     /// @brief Checks if the stack is empty (size is 0)
     /// @return True if empty, false otherwise
     bool is_empty();
 
-    /// @brief  Pushes an item to the front of the stack
+    /// @brief Returns the size of the stack
+    /// @return An integer value, representing the size
+    int size();
+
+    /// @brief  Pushes an item to the front of the stack, if there is overflow, the stack grows
     /// @param item is the item to be pushed into stack
     void push(int item);
+
+    /// @brief Pops the item from the front of the stack
+    void pop();
+
+    /// @brief Clears the stack
+    void clear();
 
     /// @brief Prints the stack data structure
     void print_stack();
